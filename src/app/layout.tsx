4deaa07a5e0ac9f464f8bb3bcf67cedf43c50e5a
SangJun Lee <molung@icloud.com>
2024-02-24 12:38:36 +0900

    fix : og 2차테스트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "../components/Header/Header";
+import "@vercel/og";
 
 export const revalidate = 0;
 export const dynamic = "force-dynamic";
@@ -11,9 +12,6 @@ export const metadata: Metadata = {
   icons: {
     icon: "./icon.png",
   },
-  openGraph: {
-    images: "./opengraph-image.png",
-  },
 };
 
 export default function RootLayout({
@@ -23,6 +21,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <meta property="og:title" content="YAPP 23th 성과공유회 방명록" />
+        <meta
+          property="og:description"
+          content="2월 24일 성과공유회, 4개월간 정말 고생 많으셨어요!!"
+        />
+        <meta property="og:image" content="/og.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="YAPP 23th 성과공유회 방명록" />
+        <meta
+          name="twitter:description"
+          content="2월 24일 성과공유회, 4개월간 정말 고생 많으셨어요!!"
+        />
+        <meta name="twitter:image" content="/og.png" />
+      </head>
       <body>
         <main>
           <Header />
