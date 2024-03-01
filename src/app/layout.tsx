c506668c3375f865dfdc1174e1b1b8a17d78d30d
SangJun Lee <molung@icloud.com>
2024-03-01 17:20:48 +0900

    fix : 오타 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export const revalidate = 0;
 export const dynamic = "force-dynamic";
 
 export const metadata: Metadata = {
-  title: "YAPP 23th 성과공유회 방명록",
+  title: "YAPP 23rd 성과공유회 방명록",
   description: "2월 24일 성과공유회, 4개월간 정말 고생 많으셨어요!!",
   icons: {
     icon: "./icon.png",
@@ -22,14 +22,14 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
-        <meta property="og:title" content="YAPP 23th 성과공유회 방명록" />
+        <meta property="og:title" content="YAPP 23rd 성과공유회 방명록" />
         <meta
           property="og:description"
           content="2월 24일 성과공유회, 4개월간 정말 고생 많으셨어요!!"
         />
         <meta property="og:image" content="/og.png" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="YAPP 23th 성과공유회 방명록" />
+        <meta name="twitter:title" content="YAPP 23rd 성과공유회 방명록" />
         <meta
           name="twitter:description"
           content="2월 24일 성과공유회, 4개월간 정말 고생 많으셨어요!!"
